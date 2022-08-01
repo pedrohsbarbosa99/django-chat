d456ceaf7af4e53f9ba5526b2977c65bb4bce9ef
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2022-08-01 15:43:14 -0300

    Asgi config updated

--- djangochatexample/asgi.py
+++ djangochatexample/asgi.py
@@ -1,19 +1,23 @@
 import os
+import sys
+
 
 import django
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatexample.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import URLRouter, ProtocolTypeRouter
-from djangochatexample.room import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
-django.setup()
+import djangochatexample.room.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            djangochatexample.room.routing.websocket_urlpatterns
         )
     )
 })
